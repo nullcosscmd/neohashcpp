75d71a8aec87511077ff161f161fa80f574784f2
nullcoss <>
2021-05-22 09:33:38 -0400

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ int main()
     }
     else
     {
-        jmethodID mid = env->GetStaticMethodID(cls, "mymain", "()V");
+        jmethodID mid = env->GetStaticMethodID(cls, "printHash", "()V");
 
         if (mid == nullptr)
         {
