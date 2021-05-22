662820900d2347def7ce2f9497f052eaa42cb982
nullcoss <>
2021-05-22 10:36:51 -0400

    Started implementing file hashing

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
+#include <fstream>
 #include "headers/hasher.hpp"
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     Hasher hasher;
 
-    hasher.hash("test");
+    fstream file;
+
+    file.open(argv[1], ios::in);
+
+    string fakeLine;
+
+    int lines = 0;
+
+    if (file.is_open())
+    {
+        while (getline(file, fakeLine))
+        {
+            lines++;
+        }
+        string line[lines];
+        lines = 0;
+        while (getline(file, fakeLine))
+        {
+            line[lines] = fakeLine;
+            lines++;
+        }
+        file.close();
+
+        const char *s1 = line->c_str();
+
+        //This hash is incorrect
+        hasher.hash(s1);
+    }
 }
